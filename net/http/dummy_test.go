83c58c6eba1a88109f4c829295bf48ea30313d78
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-03 23:48:41 +0900

    net/http: example_test.go を翻訳

---
+++ net/http/dummy_test.go
@@ -0,0 +1,17 @@
+package http_test
+
+import "net/http"
+
+type dotFileHidingFileSystem struct{ any }
+
+func (fs dotFileHidingFileSystem) Open(name string) (http.File, error)
+
+type countHandler struct{}
+
+func (h *countHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
+
+type apiHandler struct{}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
+
+func newPeopleHandler() http.Handler
