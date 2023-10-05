2de28d50e76a0c448406a578995c169cb998c2a5
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-05 16:48:37 +0900

    strings: replace.goを翻訳

--- strings/replace.go
+++ strings/replace.go
@@ -9,24 +9,23 @@ import (
 	"github.com/shogo82148/std/sync"
 )
 
-// Replacer replaces a list of strings with replacements.
-// It is safe for concurrent use by multiple goroutines.
+// Replacerは、置換物のリストで文字列を置換します。
+// 複数のゴルーチンによる同時使用に対して安全です。
 type Replacer struct {
 	once   sync.Once
 	r      replacer
 	oldnew []string
 }
 
-// NewReplacer returns a new Replacer from a list of old, new string
-// pairs. Replacements are performed in the order they appear in the
-// target string, without overlapping matches. The old string
-// comparisons are done in argument order.
+// NewReplacerは、古い文字列と新しい文字列のペアのリストから新しいReplacerを返します。
+// 置換は、対象文字列に現れる順序で実行され、重複するマッチングは行われません。
+// 古い文字列の比較は引数の順序で行われます。
 //
-// NewReplacer panics if given an odd number of arguments.
+// NewReplacerは、奇数の引数が与えられた場合にパニックを引き起こします。
 func NewReplacer(oldnew ...string) *Replacer
 
-// Replace returns a copy of s with all replacements performed.
+// Replaceは、すべての置換を実行したsのコピーを返します。
 func (r *Replacer) Replace(s string) string
 
-// WriteString writes s to w with all replacements performed.
+// WriteStringは、すべての置換を実行したsをwに書き込みます。
 func (r *Replacer) WriteString(w io.Writer, s string) (n int, err error)
