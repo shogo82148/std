ea000c7202658dcff7dbf29d736fbe4471f83bc9
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-08 18:21:45 +0900

    index/suffixarray: suffixarray.go を翻訳

--- index/suffixarray/suffixarray.go
+++ index/suffixarray/suffixarray.go
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package suffixarray implements substring search in logarithmic time using
-// an in-memory suffix array.
+// Package suffixarray は、インメモリの接尾辞配列を使用して対数時間での部分文字列検索を実装します。
 //
-// Example use:
+// 使用例:
 //
-//	// create index for some data
+//	// データに対してインデックスを作成する
 //	index := suffixarray.New(data)
 //
-//	// lookup byte slice s
-//	offsets1 := index.Lookup(s, -1) // the list of all indices where s occurs in data
-//	offsets2 := index.Lookup(s, 3)  // the list of at most 3 indices where s occurs in data
+//	// バイトスライス s を検索する
+//	offsets1 := index.Lookup(s, -1) // data 内の s の出現インデックスのリスト
+//	offsets2 := index.Lookup(s, 3)  // data 内で最大3つのインデックスでの s の出現リスト
+
 package suffixarray
 
 import (
@@ -20,37 +20,34 @@ import (
 	"github.com/shogo82148/std/regexp"
 )
 
-// Index implements a suffix array for fast substring search.
+// Indexは高速な部分文字列検索のための接尾辞配列を実装しています。
 type Index struct {
 	data []byte
 	sa   ints
 }
 
-// New creates a new Index for data.
-// Index creation time is O(N) for N = len(data).
+// Newはデータのために新しいインデックスを作成します。
+// インデックスの作成時間はO(N)であり、Nはdataの長さです。
 func New(data []byte) *Index
 
-// Read reads the index from r into x; x must not be nil.
+// Readはrからインデックスをxに読み込む。xはnilであってはならない。
 func (x *Index) Read(r io.Reader) error
 
-// Write writes the index x to w.
+// Writeはインデックスxをwに書き込みます。
 func (x *Index) Write(w io.Writer) error
 
-// Bytes returns the data over which the index was created.
-// It must not be modified.
+// Bytes はインデックスが作成されたデータを返します。
+// このデータは変更してはいけません。
 func (x *Index) Bytes() []byte
 
-// Lookup returns an unsorted list of at most n indices where the byte string s
-// occurs in the indexed data. If n < 0, all occurrences are returned.
-// The result is nil if s is empty, s is not found, or n == 0.
-// Lookup time is O(log(N)*len(s) + len(result)) where N is the
-// size of the indexed data.
+// Lookupは、バイト文字列sがインデックス付きデータに出現する最大n箇所のインデックスの非ソートリストを返します。n < 0の場合、すべての出現箇所が返されます。
+// sが空である、sが見つからない、またはn == 0の場合、結果はnilです。
+// ルックアップ時間はO(log(N)*len(s) + len(result))であり、Nはインデックス付きデータのサイズです。
 func (x *Index) Lookup(s []byte, n int) (result []int)
 
-// FindAllIndex returns a sorted list of non-overlapping matches of the
-// regular expression r, where a match is a pair of indices specifying
-// the matched slice of x.Bytes(). If n < 0, all matches are returned
-// in successive order. Otherwise, at most n matches are returned and
-// they may not be successive. The result is nil if there are no matches,
-// or if n == 0.
+// FindAllIndexは正規表現rの非重複マッチのソートされたリストを返します。
+// マッチは、x.Bytes()の一致するスライスを指定するインデックスのペアです。
+// nが0未満の場合、すべてのマッチが連続して返されます。
+// そうでない場合、最大でnマッチが返される可能性がありますが、連続しているとは限りません。
+// マッチがない場合、またはn == 0の場合は、結果はnilです。
 func (x *Index) FindAllIndex(r *regexp.Regexp, n int) (result [][]int)
