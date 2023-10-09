baef732bf8b6e6979c1ff3d6bda49922656f9c77
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-09 17:09:37 +0900

    crypto/ecdh: x25519.goを翻訳

--- crypto/ecdh/x25519.go
+++ crypto/ecdh/x25519.go
@@ -4,9 +4,8 @@
 
 package ecdh
 
-// X25519 returns a Curve which implements the X25519 function over Curve25519
-// (RFC 7748, Section 5).
+// X25519はCurve25519上でX25519関数を実装するCurveを返します
+// (RFC 7748、セクション5)。
 //
-// Multiple invocations of this function will return the same value, so it can
-// be used for equality checks and switch statements.
+// この関数の複数の呼び出しは同じ値を返すため、等価性チェックやスイッチ文に使用できます。
 func X25519() Curve
