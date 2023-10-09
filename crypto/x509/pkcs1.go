7508d65e306133733bdd1a72d1999f482e99a94f
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-09 19:13:54 +0900

    crypto/x509: pkcs1.goを翻訳

--- crypto/x509/pkcs1.go
+++ crypto/x509/pkcs1.go
@@ -8,24 +8,23 @@ import (
 	"github.com/shogo82148/std/crypto/rsa"
 )
 
-// ParsePKCS1PrivateKey parses an RSA private key in PKCS #1, ASN.1 DER form.
+// ParsePKCS1PrivateKeyはPKCS #1形式、ASN.1 DER形式のRSA秘密鍵を解析します。
 //
-// This kind of key is commonly encoded in PEM blocks of type "RSA PRIVATE KEY".
+// この種のキーは一般的に、"RSA PRIVATE KEY"というタイプのPEMブロックにエンコードされます。
 func ParsePKCS1PrivateKey(der []byte) (*rsa.PrivateKey, error)
 
-// MarshalPKCS1PrivateKey converts an RSA private key to PKCS #1, ASN.1 DER form.
+// MarshalPKCS1PrivateKeyはRSAの秘密鍵をPKCS #1、ASN.1 DER形式に変換します。
 //
-// This kind of key is commonly encoded in PEM blocks of type "RSA PRIVATE KEY".
-// For a more flexible key format which is not RSA specific, use
-// MarshalPKCS8PrivateKey.
+// この種類の鍵は、一般的には「RSA PRIVATE KEY」というタイプのPEMブロックにエンコードされます。
+// RSA固有ではなく、より柔軟な鍵形式が必要な場合は、MarshalPKCS8PrivateKeyを使用してください。
 func MarshalPKCS1PrivateKey(key *rsa.PrivateKey) []byte
 
-// ParsePKCS1PublicKey parses an RSA public key in PKCS #1, ASN.1 DER form.
+// ParsePKCS1PublicKeyはPKCS＃1、ASN.1 DER形式のRSA公開鍵を解析します。
 //
-// This kind of key is commonly encoded in PEM blocks of type "RSA PUBLIC KEY".
+// この種の鍵は、一般的に "RSA PUBLIC KEY"というタイプのPEMブロックでエンコードされています。
 func ParsePKCS1PublicKey(der []byte) (*rsa.PublicKey, error)
 
-// MarshalPKCS1PublicKey converts an RSA public key to PKCS #1, ASN.1 DER form.
+// MarshalPKCS1PublicKeyはRSA公開鍵をPKCS＃1のASN.1 DER形式に変換します。
 //
-// This kind of key is commonly encoded in PEM blocks of type "RSA PUBLIC KEY".
+// この種類の鍵は一般的に、"RSA PUBLIC KEY"タイプのPEMブロックにエンコードされます。
 func MarshalPKCS1PublicKey(key *rsa.PublicKey) []byte
