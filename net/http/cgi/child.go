3ab63b2d2ce1569e9ca129dc60e83798168e96f5
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-10 05:47:48 +0900

    net/http/cgi: child.go

--- net/http/cgi/child.go
+++ net/http/cgi/child.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This file implements CGI from the perspective of a child
-// process.
+// このファイルは子プロセスの視点からCGIを実装しています。
 
 package cgi
 
@@ -11,18 +10,14 @@ import (
 	"github.com/shogo82148/std/net/http"
 )
 
-// Request returns the HTTP request as represented in the current
-// environment. This assumes the current program is being run
-// by a web server in a CGI environment.
-// The returned Request's Body is populated, if applicable.
+// Request は現在の環境で表される HTTP リクエストを返します。
+// この関数は、現在のプログラムが CGI 環境で実行されていることを前提としています。
+// 適用される場合、返された Request の Body は取得されます。
 func Request() (*http.Request, error)
 
-// RequestFromMap creates an http.Request from CGI variables.
-// The returned Request's Body field is not populated.
+// RequestFromMapはCGI変数からhttp.Requestを作成します。
+// 返されたRequestのBodyフィールドは入力されません。
 func RequestFromMap(params map[string]string) (*http.Request, error)
 
-// Serve executes the provided Handler on the currently active CGI
-// request, if any. If there's no current CGI environment
-// an error is returned. The provided handler may be nil to use
-// http.DefaultServeMux.
+// Serveは現在アクティブなCGIリクエストに提供されたHandlerを実行します。もし現在のCGI環境がない場合、エラーが返されます。提供されたハンドラーがnilの場合、http.DefaultServeMuxが使用されます。
 func Serve(handler http.Handler) error
