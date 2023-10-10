740390b6e80368ccba675b51834c13e34727a5ac
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-11 08:45:45 +0900

    time: sleep.go

--- time/sleep.go
+++ time/sleep.go
@@ -4,14 +4,14 @@
 
 package time
 
-// Sleep pauses the current goroutine for at least the duration d.
-// A negative or zero duration causes Sleep to return immediately.
+// Sleepは現在のゴルーチンを少なくともdの期間だけ一時停止します。
+// 負数またはゼロの期間は、Sleepがすぐに戻る原因となります。
 func Sleep(d Duration)
 
-// The Timer type represents a single event.
-// When the Timer expires, the current time will be sent on C,
-// unless the Timer was created by AfterFunc.
-// A Timer must be created with NewTimer or AfterFunc.
+// Timer型は単一のイベントを表します。
+// Timerが時間切れになると、現在の時間がCに送信されます。
+// ただし、TimerがAfterFuncによって作成された場合は除きます。
+// TimerはNewTimerまたはAfterFuncで作成する必要があります。
 type Timer struct {
 	C <-chan Time
 	r runtimeTimer
@@ -41,55 +41,40 @@ type Timer struct {
 // with f explicitly.
 func (t *Timer) Stop() bool
 
-// NewTimer creates a new Timer that will send
-// the current time on its channel after at least duration d.
+// NewTimerは、少なくともdの期間経過後に、
+// そのチャネルに現在の時刻を送信する新しいTimerを作成します。
 func NewTimer(d Duration) *Timer
 
-// Reset changes the timer to expire after duration d.
-// It returns true if the timer had been active, false if the timer had
-// expired or been stopped.
+// Resetはタイマーを期間d後に期限切れにする。
+// タイマーがアクティブであった場合はtrue、期限切れまたは停止された場合はfalseを返す。
 //
-// For a Timer created with NewTimer, Reset should be invoked only on
-// stopped or expired timers with drained channels.
+// NewTimerで作成されたタイマーの場合、Resetは停止または期限切れのタイマーにのみ呼び出すべきです。チャネルが空になっている必要があります。
 //
-// If a program has already received a value from t.C, the timer is known
-// to have expired and the channel drained, so t.Reset can be used directly.
-// If a program has not yet received a value from t.C, however,
-// the timer must be stopped and—if Stop reports that the timer expired
-// before being stopped—the channel explicitly drained:
+// プログラムがすでにt.Cから値を受け取っている場合、タイマーは期限切れであり、チャネルは空になっているため、t.Resetを直接使用できます。
+// しかし、まだt.Cから値を受け取っていない場合、タイマーは停止しなければならず、Stopがタイマーが停止される前に期限切れしたことを報告した場合は、
+// チャネルを明示的に空にする必要があります：
 //
 //	if !t.Stop() {
 //		<-t.C
 //	}
 //	t.Reset(d)
 //
-// This should not be done concurrent to other receives from the Timer's
-// channel.
+// これは、他の受信操作と同時に行うべきではありません。
 //
-// Note that it is not possible to use Reset's return value correctly, as there
-// is a race condition between draining the channel and the new timer expiring.
-// Reset should always be invoked on stopped or expired channels, as described above.
-// The return value exists to preserve compatibility with existing programs.
+// Resetの返り値を正しく使用することはできないことに注意してください。チャネルの空にする操作と新しいタイマーの期限切れとの競合状態があります。
+// 上記に説明したように、Resetは必ず停止または期限切れのチャネルに対して呼び出すべきです。返り値は、既存のプログラムとの互換性を保つために存在します。
 //
-// For a Timer created with AfterFunc(d, f), Reset either reschedules
-// when f will run, in which case Reset returns true, or schedules f
-// to run again, in which case it returns false.
-// When Reset returns false, Reset neither waits for the prior f to
-// complete before returning nor does it guarantee that the subsequent
-// goroutine running f does not run concurrently with the prior
-// one. If the caller needs to know whether the prior execution of
-// f is completed, it must coordinate with f explicitly.
+// AfterFunc(d, f)で作成されたタイマーの場合、Resetはfが実行されるタイミングを再スケジュールするかどうかによってtrueまたはfalseを返します。
+// Resetがfalseを返す場合、Resetは前のfが完了するのを待たずにリターンせず、また後続のgoroutineが前のgoroutineと同時に実行されないことを保証しません。
+// 前のfが完了したかどうかを知る必要がある場合、明示的にfと調整する必要があります。
 func (t *Timer) Reset(d Duration) bool
 
-// After waits for the duration to elapse and then sends the current time
-// on the returned channel.
-// It is equivalent to NewTimer(d).C.
-// The underlying Timer is not recovered by the garbage collector
-// until the timer fires. If efficiency is a concern, use NewTimer
-// instead and call Timer.Stop if the timer is no longer needed.
+// durationが経過するのを待って、その後で現在の時刻を返されたチャネルに送信します。
+// これはNewTimer(d).Cと同等です。
+// ガベージコレクタによってTimerが回収されるのは、タイマーが発火するまでではありません。
+// 効率が問題の場合は、代わりにNewTimerを使用し、タイマーが不要になった場合はTimer.Stopを呼び出してください。
 func After(d Duration) <-chan Time
 
-// AfterFunc waits for the duration to elapse and then calls f
-// in its own goroutine. It returns a Timer that can
-// be used to cancel the call using its Stop method.
+// AfterFuncは、指定した時間が経過した後、fを自身のゴルーチンで呼び出します。
+// Stopメソッドを使用して呼び出しをキャンセルするために使用できるTimerを返します。
 func AfterFunc(d Duration, f func()) *Timer
