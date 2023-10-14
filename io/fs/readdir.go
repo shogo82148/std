ced17dd7401e585072f4f181089e20ada5881a7e
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-14 12:21:50 +0900

    io/fs: readdir.goのコンフリクト解消

--- io/fs/readdir.go
+++ io/fs/readdir.go
@@ -4,12 +4,7 @@
 
 package fs
 
-<<<<<<< HEAD
-// ReadDirFS is the interface implemented by a file system
-// that provides an optimized implementation of [ReadDir].
-=======
-// ReadDirFSは、ReadDirの最適化された実装を提供するファイルシステムで実装されるインターフェースです。
->>>>>>> release-branch.go1.21
+// ReadDirFSは、 [ReadDir] の最適化された実装を提供するファイルシステムで実装されるインターフェースです。
 type ReadDirFS interface {
 	FS
 
@@ -19,20 +14,10 @@ type ReadDirFS interface {
 // ReadDirは指定されたディレクトリを読み取り、
 // ファイル名でソートされたディレクトリエントリのリストを返します。
 //
-<<<<<<< HEAD
-// If fs implements [ReadDirFS], ReadDir calls fs.ReadDir.
-// Otherwise ReadDir calls fs.Open and uses ReadDir and Close
-// on the returned file.
-func ReadDir(fsys FS, name string) ([]DirEntry, error)
-
-// FileInfoToDirEntry returns a [DirEntry] that returns information from info.
-// If info is nil, FileInfoToDirEntry returns nil.
-=======
-// fsがReadDirFSを実装している場合、ReadDirはfs.ReadDirを呼び出します。
+// fsが [ReadDirFS] を実装している場合、ReadDirはfs.ReadDirを呼び出します。
 // そうでない場合、ReadDirはfs.Openを呼び出し、返されたファイルでReadDirとCloseを使用します。
 func ReadDir(fsys FS, name string) ([]DirEntry, error)
 
-// FileInfoToDirEntryは、infoから情報を返すDirEntryを返します。
+// FileInfoToDirEntryは、infoから情報を返す [DirEntry] を返します。
 // もしinfoがnilの場合、FileInfoToDirEntryはnilを返します。
->>>>>>> release-branch.go1.21
 func FileInfoToDirEntry(info FileInfo) DirEntry
