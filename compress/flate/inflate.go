9ece0c4fefee7d409443ce0f85e22cdaaf9e5feb
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-14 10:02:40 +0900

    compress/flate: infate.goのコンフリクト解消

--- compress/flate/inflate.go
+++ compress/flate/inflate.go
@@ -40,26 +40,14 @@ type WriteError struct {
 
 func (e *WriteError) Error() string
 
-<<<<<<< HEAD
-// ResetterはNewReaderまたはNewReaderDictが返すReadCloserをリセットし、新しい基になるReaderに切り替えます。これにより、新しいものを割り当てる代わりにReadCloserを再利用することができます。
-=======
-// Resetter resets a ReadCloser returned by [NewReader] or [NewReaderDict]
-// to switch to a new underlying [Reader]. This permits reusing a ReadCloser
-// instead of allocating a new one.
->>>>>>> upstream/master
+// Resetterは [NewReader] または [NewReaderDict] が返すReadCloserをリセットし、新しい基になる [Reader] に切り替えます。これにより、新しいものを割り当てる代わりにReadCloserを再利用することができます。
 type Resetter interface {
 	Reset(r io.Reader, dict []byte) error
 }
 
-<<<<<<< HEAD
-// NewReader で必要とされる実際の読み取りインターフェース。
+// [NewReader] で必要とされる実際の読み取りインターフェース。
 // 渡された io.Reader が ReadByte も持っていない場合、
-// NewReader は自身のバッファリングを導入します。
-=======
-// The actual read interface needed by [NewReader].
-// If the passed in io.Reader does not also have ReadByte,
-// the [NewReader] will introduce its own buffering.
->>>>>>> upstream/master
+// [NewReader] は自身のバッファリングを導入します。
 type Reader interface {
 	io.Reader
 	io.ByteReader
@@ -72,26 +60,13 @@ type Reader interface {
 // The reader returns [io.EOF] after the final block in the DEFLATE stream has
 // been encountered. Any trailing data after the final block is ignored.
 //
-<<<<<<< HEAD
-// NewReaderによって返されるReadCloserは、Resetterも実装しています。
+// NewReaderによって返されるReadCloserは、 [Resetter] も実装しています。
 func NewReader(r io.Reader) io.ReadCloser
 
-// NewReaderDictはNewReaderと同じようにリーダーを初期化しますが、
+// NewReaderDictは [NewReader] と同じようにリーダーを初期化しますが、
 // 事前に設定された辞書でリーダーを初期化します。
 // 返されたリーダーは、与えられた辞書で圧縮解除されたデータストリームが開始されたかのように振る舞います。
 // この辞書は既に読み取られています。通常、NewWriterDictで圧縮されたデータを読み込むためにNewReaderDictが使用されます。
 //
-// NewReaderによって返されたReadCloserはResetterも実装しています。
-=======
-// The ReadCloser returned by NewReader also implements [Resetter].
-func NewReader(r io.Reader) io.ReadCloser
-
-// NewReaderDict is like [NewReader] but initializes the reader
-// with a preset dictionary. The returned [Reader] behaves as if
-// the uncompressed data stream started with the given dictionary,
-// which has already been read. NewReaderDict is typically used
-// to read data compressed by NewWriterDict.
-//
-// The ReadCloser returned by NewReaderDict also implements [Resetter].
->>>>>>> upstream/master
+// NewReaderによって返されたReadCloserは [Resetter] も実装しています。
 func NewReaderDict(r io.Reader, dict []byte) io.ReadCloser
