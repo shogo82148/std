839d2ad001b506df8f13bbb669cbffd60ccb5813
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-14 10:49:19 +0900

    sort: slice.gonおコンフリクト解消

--- sort/slice.go
+++ sort/slice.go
@@ -11,38 +11,21 @@ package sort
 // 元の順序から逆になる場合があります。
 // 安定したソートをするには、SliceStableを使用してください。
 //
-<<<<<<< HEAD
-// The less function must satisfy the same requirements as
-// the Interface type's Less method.
+// less関数は、Interface型のLessメソッドと同じ要件を満たす必要があります。
 //
-// Note: in many situations, the newer slices.SortFunc function is more
-// ergonomic and runs faster.
-=======
-// less関数は、Interface型のLessメソッドと同様の要件を満たす必要があります。
->>>>>>> release-branch.go1.21
+// 注意：多くの場合、より新しいslices.SortFunc関数の方が操作性が高く、実行速度も速くなります。
 func Slice(x any, less func(i, j int) bool)
 
 // SliceStableは、与えられた比較関数を使用してスライスxをソートし、等しい要素を元の順序で保持します。
 // xがスライスでない場合、パニックを起こします。
 //
-<<<<<<< HEAD
-// The less function must satisfy the same requirements as
-// the Interface type's Less method.
+// less関数は、Interface型のLessメソッドと同じ要件を満たす必要があります。
 //
-// Note: in many situations, the newer slices.SortStableFunc function is more
-// ergonomic and runs faster.
+// 注意：多くの場合、より新しいslices.SortStableFunc関数の方が操作性が高く、実行速度も速くなります。
 func SliceStable(x any, less func(i, j int) bool)
 
-// SliceIsSorted reports whether the slice x is sorted according to the provided less function.
-// It panics if x is not a slice.
+// SliceIsSortedは、提供されたless関数に従ってスライスxがソートされているかどうかを報告します。
+// xがスライスでない場合、panicします。
 //
-// Note: in many situations, the newer slices.IsSortedFunc function is more
-// ergonomic and runs faster.
-=======
-// 比較関数は、Interface型のLessメソッドと同じ要件を満たす必要があります。
-func SliceStable(x any, less func(i, j int) bool)
-
-// SliceIsSortedは、与えられたless関数に従ってスライスxがソートされているかどうかを報告します。
-// スライスxがスライスでない場合、パニックを起こします。
->>>>>>> release-branch.go1.21
+// 注意：多くの場合、より新しいslices.IsSortedFunc関数の方が操作性が高く、実行速度も速くなります。
 func SliceIsSorted(x any, less func(i, j int) bool) bool
