1a71e4d77084aaea08337788cd202e5ce9424d09
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-14 12:22:47 +0900

    io/fs: stat.goのコンフリクト解消

--- io/fs/stat.go
+++ io/fs/stat.go
@@ -11,15 +11,8 @@ type StatFS interface {
 	Stat(name string) (FileInfo, error)
 }
 
-<<<<<<< HEAD
-// Stat returns a [FileInfo] describing the named file from the file system.
+// Statはファイルシステムから指定されたファイルに関する [FileInfo] を返します。
 //
-// If fs implements [StatFS], Stat calls fs.Stat.
-// Otherwise, Stat opens the [File] to stat it.
-=======
-// Statはファイルシステムから指定されたファイルに関するFileInfoを返します。
-//
-// もしfsがStatFSを実装している場合、Statはfs.Statを呼び出します。
-// そうでない場合、Statはファイルを開いて統計情報を取得します。
->>>>>>> release-branch.go1.21
+// もしfsが [StatFS] を実装している場合、Statはfs.Statを呼び出します。
+// そうでない場合、Statは [File] を開いて統計情報を取得します。
 func Stat(fsys FS, name string) (FileInfo, error)
