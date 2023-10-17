a78da75256992ec55713212dc7df5feedaebba4b
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-18 08:17:49 +0900

    hash/crc64: コンフリクト解消

--- hash/crc64/crc64.go
+++ hash/crc64/crc64.go
@@ -25,32 +25,15 @@ const (
 // Table は効率的な処理のための多項式を表す256単語のテーブルです。
 type Table [256]uint64
 
-<<<<<<< HEAD
-// MakeTable は指定された多項式から構築されたTableを返します。
+// MakeTable は指定された多項式から構築された [Table] を返します。
 // このTableの内容は変更してはなりません。
 func MakeTable(poly uint64) *Table
 
-// NewはTableで表される多項式を使用してCRC-64チェックサムを計算する新しいhash.Hash64を作成します。Sumメソッドは値をビッグエンディアンのバイト順で並べます。返されるHash64は、内部状態をmarshalおよびunmarshalするためのencoding.BinaryMarshalerおよびencoding.BinaryUnmarshalerも実装しています。
-=======
-// MakeTable returns a [Table] constructed from the specified polynomial.
-// The contents of this [Table] must not be modified.
-func MakeTable(poly uint64) *Table
-
-// New creates a new hash.Hash64 computing the CRC-64 checksum using the
-// polynomial represented by the [Table]. Its Sum method will lay the
-// value out in big-endian byte order. The returned Hash64 also
-// implements [encoding.BinaryMarshaler] and [encoding.BinaryUnmarshaler] to
-// marshal and unmarshal the internal state of the hash.
->>>>>>> upstream/master
+// Newは [Table] で表される多項式を使用してCRC-64チェックサムを計算する新しいhash.Hash64を作成します。Sumメソッドは値をビッグエンディアンのバイト順で並べます。返されるHash64は、内部状態をmarshalおよびunmarshalするための [encoding.BinaryMarshaler] および [encoding.BinaryUnmarshaler] も実装しています。
 func New(tab *Table) hash.Hash64
 
 // Updateはpのバイトをcrcに追加した結果を返します。
 func Update(crc uint64, tab *Table, p []byte) uint64
 
-<<<<<<< HEAD
-// Checksum 関数は、Table が表す多項式を使って、データの CRC-64 チェックサムを返します。
-=======
-// Checksum returns the CRC-64 checksum of data
-// using the polynomial represented by the [Table].
->>>>>>> upstream/master
+// Checksum 関数は、 [Table] が表す多項式を使って、データの CRC-64 チェックサムを返します。
 func Checksum(data []byte, tab *Table) uint64
