cc4ccf60376a714753b016889b34624f31c91fda
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-22 11:46:03 +0900

    encoding/base64: 翻訳

--- encoding/base64/example_test.go
+++ encoding/base64/example_test.go
@@ -74,9 +74,8 @@ func ExampleNewEncoder() {
 	input := []byte("foo\x00bar")
 	encoder := base64.NewEncoder(base64.StdEncoding, os.Stdout)
 	encoder.Write(input)
-	// Must close the encoder when finished to flush any partial blocks.
-	// If you comment out the following line, the last partial block "r"
-	// won't be encoded.
+	// 部分的なブロックをフラッシュするために、終了時にエンコーダーを閉じる必要があります。
+	// 次の行のコメントを外すと、最後の部分ブロック「r」がエンコードされなくなります。
 	encoder.Close()
 	// Output:
 	// Zm9vAGJhcg==
