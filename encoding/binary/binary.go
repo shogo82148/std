cb7f6f19fda6188598b1752c3d3e33dfa96f96b3
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-22 21:27:17 +0900

    encoding/binary: コンフリクト解消

--- encoding/binary/binary.go
+++ encoding/binary/binary.go
@@ -14,30 +14,18 @@
 // 仕様については、以下を参照してください。
 // https://developers.google.com/protocol-buffers/docs/encoding。
 //
-<<<<<<< HEAD
-// This package favors simplicity over efficiency. Clients that require
-// high-performance serialization, especially for large data structures,
-// should look at more advanced solutions such as the [encoding/gob]
-// package or [google.golang.org/protobuf] for protocol buffers.
-=======
 // このパッケージは、効率よりもシンプルさを重視しています。
 // 特に大規模なデータ構造に対して高性能なシリアル化が必要なクライアントは、
-// encoding/gobパッケージやプロトコルバッファなどのより高度なソリューションを検討する必要があります。
->>>>>>> release-branch.go1.21
+// [encoding/gob] パッケージやプロトコルバッファ [google.golang.org/protobuf] などのより高度なソリューションを検討する必要があります。
 package binary
 
 import (
 	"github.com/shogo82148/std/io"
 )
 
-<<<<<<< HEAD
-// A ByteOrder specifies how to convert byte slices into
-// 16-, 32-, or 64-bit unsigned integers.
-//
-// It is implemented by [LittleEndian], [BigEndian], and [NativeEndian].
-=======
 // ByteOrderは、バイトスライスを16、32、または64ビットの符号なし整数に変換する方法を指定します。
->>>>>>> release-branch.go1.21
+//
+// これは [LittleEndian]、[BigEndian]、および [NativeEndian] によって実装されています。
 type ByteOrder interface {
 	Uint16([]byte) uint16
 	Uint32([]byte) uint32
@@ -48,14 +36,9 @@ type ByteOrder interface {
 	String() string
 }
 
-<<<<<<< HEAD
-// AppendByteOrder specifies how to append 16-, 32-, or 64-bit unsigned integers
-// into a byte slice.
-//
-// It is implemented by [LittleEndian], [BigEndian], and [NativeEndian].
-=======
 // AppendByteOrderは、16、32、または64ビットの符号なし整数をバイトスライスに追加する方法を指定します。
->>>>>>> release-branch.go1.21
+//
+// これは [LittleEndian]、[BigEndian]、および [NativeEndian] によって実装されています。
 type AppendByteOrder interface {
 	AppendUint16([]byte, uint16) []byte
 	AppendUint32([]byte, uint32) []byte
@@ -63,17 +46,10 @@ type AppendByteOrder interface {
 	String() string
 }
 
-<<<<<<< HEAD
-// LittleEndian is the little-endian implementation of [ByteOrder] and [AppendByteOrder].
+// LittleEndianは、[ByteOrder] および [AppendByteOrder] のリトルエンディアン実装です。
 var LittleEndian littleEndian
 
-// BigEndian is the big-endian implementation of [ByteOrder] and [AppendByteOrder].
-=======
-// LittleEndianは、ByteOrderおよびAppendByteOrderのリトルエンディアン実装です。
-var LittleEndian littleEndian
-
-// BigEndianは、ByteOrderおよびAppendByteOrderのビッグエンディアン実装です。
->>>>>>> release-branch.go1.21
+// BigEndianは、[ByteOrder] および [AppendByteOrder] のビッグエンディアン実装です。
 var BigEndian bigEndian
 
 // Readは、rからdataに対して構造化されたバイナリデータを読み取ります。
@@ -87,15 +63,9 @@ var BigEndian bigEndian
 // 構造体に読み込む場合、すべての非ブランクフィールドはエクスポートされている必要があります。
 // そうでない場合、Readはパニックを引き起こす可能性があります。
 //
-<<<<<<< HEAD
-// The error is [io.EOF] only if no bytes were read.
-// If an [io.EOF] happens after reading some but not all the bytes,
-// Read returns [io.ErrUnexpectedEOF].
-=======
-// エラーがEOFであるのは、バイトが読み込まれなかった場合のみです。
-// 一部のバイトが読み込まれた後にEOFが発生した場合、
-// ReadはErrUnexpectedEOFを返します。
->>>>>>> release-branch.go1.21
+// エラーが [io.EOF] であるのは、バイトが読み込まれなかった場合のみです。
+// 一部のバイトが読み込まれた後に [io.EOF] が発生した場合、
+// Readは [io.ErrUnexpectedEOF] を返します。
 func Read(r io.Reader, order ByteOrder, data any) error
 
 // Writeは、データのバイナリ表現をwに書き込みます。
@@ -106,13 +76,7 @@ func Read(r io.Reader, order ByteOrder, data any) error
 // 構造体を書き込む場合、ブランク（_）フィールド名を持つフィールドのデータはゼロ値で書き込まれます。
 func Write(w io.Writer, order ByteOrder, data any) error
 
-<<<<<<< HEAD
-// Size returns how many bytes [Write] would generate to encode the value v, which
-// must be a fixed-size value or a slice of fixed-size values, or a pointer to such data.
-// If v is neither of these, Size returns -1.
-=======
-// Sizeは、値vをエンコードするためにWriteが生成するバイト数を返します。
+// Sizeは、値vをエンコードするために [Write] が生成するバイト数を返します。
 // vは、固定サイズの値または固定サイズの値のスライス、またはそのようなデータへのポインタである必要があります。
 // vがこれらのいずれでもない場合、Sizeは-1を返します。
->>>>>>> release-branch.go1.21
 func Size(v any) int
