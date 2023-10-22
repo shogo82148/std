28e00b01f3e0568dfab3b969b228389e480dac0f
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-22 16:25:55 +0900

    go/scanner: コンフリクト解消

--- go/scanner/errors.go
+++ go/scanner/errors.go
@@ -9,15 +9,8 @@ import (
 	"github.com/shogo82148/std/io"
 )
 
-<<<<<<< HEAD
-// ErrorListでは、エラーは*Errorで表されます。
+// [ErrorList] では、エラーは*Errorで表されます。
 // Posは、有効な場合は問題のあるトークンの先頭を指し、エラーの状況はMsgで説明されます。
-=======
-// In an [ErrorList], an error is represented by an *Error.
-// The position Pos, if valid, points to the beginning of
-// the offending token, and the error condition is described
-// by Msg.
->>>>>>> upstream/master
 type Error struct {
 	Pos token.Position
 	Msg string
@@ -30,58 +23,30 @@ func (e Error) Error() string
 // ErrorListのゼロ値は使用する準備ができた空のErrorListです。
 type ErrorList []*Error
 
-<<<<<<< HEAD
-// Addは、指定された位置とエラーメッセージを持つエラーをErrorListに追加します。
+// Addは、指定された位置とエラーメッセージを持つ [Error] を [ErrorList] に追加します。
 func (p *ErrorList) Add(pos token.Position, msg string)
 
-// ResetはErrorListのエラーをリセットします。
+// Resetは [ErrorList] のエラーをリセットします。
 func (p *ErrorList) Reset()
 
-// ErrorListはsort Interfaceを実装します。
-=======
-// Add adds an [Error] with given position and error message to an [ErrorList].
-func (p *ErrorList) Add(pos token.Position, msg string)
-
-// Reset resets an [ErrorList] to no errors.
-func (p *ErrorList) Reset()
-
-// [ErrorList] implements the sort Interface.
->>>>>>> upstream/master
+// [ErrorList] はsort Interfaceを実装します。
 func (p ErrorList) Len() int
 func (p ErrorList) Swap(i, j int)
 
 func (p ErrorList) Less(i, j int) bool
 
-<<<<<<< HEAD
-// Sort関数は、ErrorListをソートします。*Errorのエントリは位置で、他のエラーはエラーメッセージでソートされ、*Errorのエントリの前に配置されます。
+// Sort関数は、[ErrorList] をソートします。*[Error] のエントリは位置で、他のエラーはエラーメッセージでソートされ、*[Error] のエントリの前に配置されます。
 func (p ErrorList) Sort()
 
-// RemoveMultiplesはErrorListをソートし、1行ごとに最初のエラー以外を削除します。
+// RemoveMultiplesは [ErrorList] をソートし、1行ごとに最初のエラー以外を削除します。
 func (p *ErrorList) RemoveMultiples()
 
-// ErrorListはerrorインターフェースを実装しています。
-=======
-// Sort sorts an [ErrorList]. *[Error] entries are sorted by position,
-// other errors are sorted by error message, and before any *[Error]
-// entry.
-func (p ErrorList) Sort()
-
-// RemoveMultiples sorts an [ErrorList] and removes all but the first error per line.
-func (p *ErrorList) RemoveMultiples()
-
-// An [ErrorList] implements the error interface.
->>>>>>> upstream/master
+// [ErrorList] はerrorインターフェースを実装しています。
 func (p ErrorList) Error() string
 
 // Errはこのエラーリストに相当するエラーを返します。
 // リストが空の場合、Errはnilを返します。
 func (p ErrorList) Err() error
 
-<<<<<<< HEAD
-// PrintErrorは、errパラメータがErrorListの場合、エラーリストを1行ごとにwに出力します。それ以外の場合は、err文字列を出力します。
-=======
-// PrintError is a utility function that prints a list of errors to w,
-// one error per line, if the err parameter is an [ErrorList]. Otherwise
-// it prints the err string.
->>>>>>> upstream/master
+// PrintErrorは、errパラメータが [ErrorList] の場合、エラーリストを1行ごとにwに出力します。それ以外の場合は、err文字列を出力します。
 func PrintError(w io.Writer, err error)
--- go/scanner/scanner.go
+++ go/scanner/scanner.go
@@ -10,22 +10,10 @@ import (
 	"github.com/shogo82148/std/go/token"
 )
 
-<<<<<<< HEAD
-// ErrorHandlerはScanner.Initに提供することができます。構文エラーが発生し、ハンドラがインストールされている場合、ハンドラは位置とエラーメッセージとともに呼び出されます。位置は問題のあるトークンの始まりを指します。
+// ErrorHandlerは [Scanner.Init] に提供することができます。構文エラーが発生し、ハンドラがインストールされている場合、ハンドラは位置とエラーメッセージとともに呼び出されます。位置は問題のあるトークンの始まりを指します。
 type ErrorHandler func(pos token.Position, msg string)
 
-// スキャナは、指定されたテキストを処理する間、スキャンの内部状態を保持します。それは他のデータ構造の一部として割り当てられることもできますが、使用前にInitによって初期化される必要があります。
-=======
-// An ErrorHandler may be provided to [Scanner.Init]. If a syntax error is
-// encountered and a handler was installed, the handler is called with a
-// position and an error message. The position points to the beginning of
-// the offending token.
-type ErrorHandler func(pos token.Position, msg string)
-
-// A Scanner holds the scanner's internal state while processing
-// a given text. It can be allocated as part of another data
-// structure but must be initialized via [Scanner.Init] before use.
->>>>>>> upstream/master
+// スキャナは、指定されたテキストを処理する間、スキャンの内部状態を保持します。それは他のデータ構造の一部として割り当てられることもできますが、使用前に [Scanner.Init] によって初期化される必要があります。
 type Scanner struct {
 	// 変更不可な状態
 	file *token.File
@@ -60,58 +48,18 @@ const (
 // 同じファイルを再使用しても問題ありません。もしファイルのサイズがsrcのサイズと一致しない場合は、
 // Initはパニックを引き起こします。
 //
-<<<<<<< HEAD
-// Scanの呼び出しは、構文エラーが発生した場合にエラーハンドラerrを呼び出し、errがnilでない場合にはエラーハンドラerrを呼び出します。
-// また、エラーが発生する毎にスキャナのErrorCountフィールドが1増加します。モードパラメーターはコメントの扱い方を決定します。
-=======
-// Calls to [Scanner.Scan] will invoke the error handler err if they encounter a
-// syntax error and err is not nil. Also, for each error encountered,
-// the [Scanner] field ErrorCount is incremented by one. The mode parameter
-// determines how comments are handled.
->>>>>>> upstream/master
+// [Scanner.Scan] の呼び出しは、構文エラーが発生した場合にエラーハンドラerrを呼び出し、errがnilでない場合にはエラーハンドラerrを呼び出します。
+// また、エラーが発生する毎に [Scanner] のErrorCountフィールドが1増加します。モードパラメーターはコメントの扱い方を決定します。
 //
 // Initはファイルの最初の文字にエラーがある場合、errを呼び出す場合があります。
 func (s *Scanner) Init(file *token.File, src []byte, err ErrorHandler, mode Mode)
 
-<<<<<<< HEAD
-// Scanは次のトークンをスキャンして、トークンの位置、トークン、およびリテラル文字列（適用可能な場合）を返します。ソースの終了はtoken.EOFで示されます。
-// 返されるトークンがリテラル（token.IDENT、token.INT、token.FLOAT、token.IMAG、token.CHAR、token.STRING）またはtoken.COMMENTである場合、リテラル文字列は対応する値を持ちます。
+// Scanは次のトークンをスキャンして、トークンの位置、トークン、およびリテラル文字列（適用可能な場合）を返します。ソースの終了は [token.EOF] で示されます。
+// 返されるトークンがリテラル（[token.IDENT]、[token.INT]、[token.FLOAT]、[token.IMAG]、[token.CHAR]、[token.STRING]）または [token.COMMENT] である場合、リテラル文字列は対応する値を持ちます。
 // 返されるトークンがキーワードの場合、リテラル文字列はキーワードです。
-// 返されるトークンがtoken.SEMICOLONの場合、対応するリテラル文字列は、ソースにセミコロンが存在する場合は";"であり、改行またはEOFの場合は"\n"です。
-// 返されるトークンがtoken.ILLEGALの場合、リテラル文字列は違反している文字です。
+// 返されるトークンが [token.SEMICOLON] の場合、対応するリテラル文字列は、ソースにセミコロンが存在する場合は";"であり、改行またはEOFの場合は"\n"です。
+// 返されるトークンが [token.ILLEGAL] の場合、リテラル文字列は違反している文字です。
 // その他の場合、Scanは空のリテラル文字列を返します。
 // より許容度の高い解析のために、構文エラーが発生した場合でも、可能な限り有効なトークンを返します。したがって、結果のトークンのシーケンスに違法なトークンが含まれていない場合でも、クライアントはエラーが発生したとは推測できません。代わりに、スキャナーのErrorCountまたはエラーハンドラーの呼び出し回数を確認する必要があります（インストールされている場合）。
 // ScanはInitでファイルセットに追加されたファイルに行情報を追加します。トークンの位置はそのファイルとファイルセットに対して相対的です。
-=======
-// Scan scans the next token and returns the token position, the token,
-// and its literal string if applicable. The source end is indicated by
-// [token.EOF].
-//
-// If the returned token is a literal ([token.IDENT], [token.INT], [token.FLOAT],
-// [token.IMAG], [token.CHAR], [token.STRING]) or [token.COMMENT], the literal string
-// has the corresponding value.
-//
-// If the returned token is a keyword, the literal string is the keyword.
-//
-// If the returned token is [token.SEMICOLON], the corresponding
-// literal string is ";" if the semicolon was present in the source,
-// and "\n" if the semicolon was inserted because of a newline or
-// at EOF.
-//
-// If the returned token is [token.ILLEGAL], the literal string is the
-// offending character.
-//
-// In all other cases, Scan returns an empty literal string.
-//
-// For more tolerant parsing, Scan will return a valid token if
-// possible even if a syntax error was encountered. Thus, even
-// if the resulting token sequence contains no illegal tokens,
-// a client may not assume that no error occurred. Instead it
-// must check the scanner's ErrorCount or the number of calls
-// of the error handler, if there was one installed.
-//
-// Scan adds line information to the file added to the file
-// set with Init. Token positions are relative to that file
-// and thus relative to the file set.
->>>>>>> upstream/master
 func (s *Scanner) Scan() (pos token.Pos, tok token.Token, lit string)
