d178d62344b8299584834dd4ffc707fd85bdd1c2
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-02 22:40:03 +0900

    math/rand/v2: pcg.go翻訳

--- math/rand/v2/pcg.go
+++ math/rand/v2/pcg.go
@@ -4,24 +4,24 @@
 
 package rand
 
-// A PCG is a PCG generator with 128 bits of internal state.
-// A zero PCG is equivalent to NewPCG(0, 0).
+// PCGは、128ビットの内部状態を持つPCGジェネレータです。
+// ゼロのPCGは、NewPCG(0, 0)と同等です。
 type PCG struct {
 	hi uint64
 	lo uint64
 }
 
-// NewPCG returns a new PCG seeded with the given values.
+// NewPCGは、与えられた値でシードされた新しいPCGを返します。
 func NewPCG(seed1, seed2 uint64) *PCG
 
-// Seed resets the PCG to behave the same way as NewPCG(seed1, seed2).
+// Seedは、PCGをNewPCG(seed1, seed2)と同じように動作するようにリセットします。
 func (p *PCG) Seed(seed1, seed2 uint64)
 
-// MarshalBinary implements the encoding.BinaryMarshaler interface.
+// MarshalBinaryは、encoding.BinaryMarshalerインターフェースを実装します。
 func (p *PCG) MarshalBinary() ([]byte, error)
 
-// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+// UnmarshalBinaryは、encoding.BinaryUnmarshalerインターフェースを実装します。
 func (p *PCG) UnmarshalBinary(data []byte) error
 
-// Uint64 return a uniformly-distributed random uint64 value.
+// Uint64は、一様に分布したランダムなuint64の値を返します。
 func (p *PCG) Uint64() uint64
