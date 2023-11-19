2c5b147273578d6a48515c5869a63d8398d793d4
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-19 21:43:34 +0900

    runtime/debug: コンフリクト解消

--- runtime/debug/stack.go
+++ runtime/debug/stack.go
@@ -8,11 +8,6 @@ package debug
 // PrintStackはruntime.Stackによって返されたスタックトレースを標準エラー出力に出力します。
 func PrintStack()
 
-<<<<<<< HEAD
 // Stackはそれを呼び出すgoroutineのフォーマットされたスタックトレースを返します。
-// runtime.Stackを呼び出して、トレース全体をキャプチャする十分に大きなバッファを使用します。
-=======
-// Stack returns a formatted stack trace of the goroutine that calls it.
-// It calls [runtime.Stack] with a large enough buffer to capture the entire trace.
->>>>>>> upstream/master
+// [runtime.Stack] を呼び出して、トレース全体をキャプチャする十分に大きなバッファを使用します。
 func Stack() []byte
