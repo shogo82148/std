642321ddc117dc24f612636708e461ba688f534d
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-23 17:25:07 +0900

    crypto/tls: コンフリクト解消

--- crypto/tls/common.go
+++ crypto/tls/common.go
@@ -96,21 +96,14 @@ type ConnectionState struct {
 	ekm func(label string, context []byte, length int) ([]byte, error)
 }
 
-<<<<<<< HEAD
-// ExportKeyingMaterialは、RFC 5705で定義されているように、エクスポートされたキーマテリアルの長さのバイトを新しいスライスで返します。contextがnilの場合、シードの一部として使用されません。Config.Renegotiation経由で再協議が許可された接続の場合、この関数はエラーを返します。
-// 返された値が接続に固有でない場合、条件があります。RFC 5705およびRFC 7627のセキュリティに関する考慮事項セクション、およびhttps://mitls.org/pages/attacks/3SHAKE#channelbindingsを参照してください。
-=======
-// ExportKeyingMaterial returns length bytes of exported key material in a new
-// slice as defined in RFC 5705. If context is nil, it is not used as part of
-// the seed. If the connection was set to allow renegotiation via
-// Config.Renegotiation, or if the connections supports neither TLS 1.3 nor
-// Extended Master Secret, this function will return an error.
+// ExportKeyingMaterialは、RFC 5705で定義されているように、新しいスライスにlengthバイトの
+// エクスポートされた鍵マテリアルを返します。contextがnilの場合、それはシードの一部として使用されません。
+// 接続がConfig.Renegotiationを介して再交渉を許可するように設定されていた場合、または接続がTLS 1.3も
+// Extended Master Secretもサポートしていない場合、この関数はエラーを返します。
 //
-// Exporting key material without Extended Master Secret or TLS 1.3 was disabled
-// in Go 1.22 due to security issues (see the Security Considerations sections
-// of RFC 5705 and RFC 7627), but can be re-enabled with the GODEBUG setting
-// tlsunsafeekm=1.
->>>>>>> upstream/master
+// Extended Master SecretまたはTLS 1.3なしでの鍵マテリアルのエクスポートは、
+// セキュリティ上の問題（RFC 5705とRFC 7627のセキュリティ考慮事項セクションを参照）により、
+// Go 1.22で無効化されましたが、GODEBUG設定tlsunsafeekm=1で再度有効化できます。
 func (cs *ConnectionState) ExportKeyingMaterial(label string, context []byte, length int) ([]byte, error)
 
 // ClientAuthTypeは、TLSクライアント認証に関するサーバーのポリシーを宣言します。
