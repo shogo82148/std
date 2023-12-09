56d86e88ebc7d1e886e03dd24f6876774844e2c8
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-10 00:52:37 +0900

    testing/slogtest: コンフリクト解消

--- testing/slogtest/slogtest.go
+++ testing/slogtest/slogtest.go
@@ -10,24 +10,9 @@ import (
 	"github.com/shogo82148/std/testing"
 )
 
-<<<<<<< HEAD
-// TestHandler tests a [slog.Handler].
-// If TestHandler finds any misbehaviors, it returns an error for each,
-// combined into a single error with [errors.Join].
-//
-// TestHandler installs the given Handler in a [slog.Logger] and
-// makes several calls to the Logger's output methods.
-// The Handler should be enabled for levels Info and above.
-//
-// The results function is invoked after all such calls.
-// It should return a slice of map[string]any, one for each call to a Logger output method.
-// The keys and values of the map should correspond to the keys and values of the Handler's
-// output. Each group in the output should be represented as its own nested map[string]any.
-// The standard keys [slog.TimeKey], [slog.LevelKey] and [slog.MessageKey] should be used.
-=======
 // TestHandlerは、[slog.Handler] をテストします。
 // もしTestHandlerが何かしらの不適切な動作を見つけた場合、それら全てを報告するエラーを返します。
-// これらはerrors.Joinを使用して単一のエラーに結合されます。
+// これらは [errors.Join] を使用して単一のエラーに結合されます。
 //
 // TestHandlerは、指定されたHandlerを [slog.Logger] にインストールし、
 // Loggerの出力メソッドに対して複数回の呼び出しを行います。
@@ -36,8 +21,7 @@ import (
 // それはmap[string]anyのスライスを返すべきで、Loggerの出力メソッドへの各呼び出しに対して一つです。
 // マップのキーと値は、Handlerの出力のキーと値に対応しているべきです。
 // 出力内の各グループは、それ自体がネストしたmap[string]anyとして表現されるべきです。
-// 標準のキーであるslog.TimeKey、slog.LevelKey、slog.MessageKeyを使用すべきです。
->>>>>>> release-branch.go1.21
+// 標準のキーである [slog.TimeKey]、[slog.LevelKey]、[slog.MessageKey] を使用すべきです。
 //
 // もしHandlerがJSONを出力するなら、`map[string]any`を引数にして
 // [encoding/json.Unmarshal] を呼び出すことで、適切なデータ構造が作成されます。
