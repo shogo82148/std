388614f0923c79c6b4df0509c95dfa7107386c0c
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-10 22:40:34 +0900

    image/draw: コンフリクト解消

--- image/draw/draw.go
+++ image/draw/draw.go
@@ -19,16 +19,9 @@ type Image interface {
 	Set(x, y int, c color.Color)
 }
 
-<<<<<<< HEAD
-// RGBA64Image extends both the [Image] and [image.RGBA64Image] interfaces with a
-// SetRGBA64 method to change a single pixel. SetRGBA64 is equivalent to
-// calling Set, but it can avoid allocations from converting concrete color
-// types to the [color.Color] interface type.
-=======
-// RGBA64Imageは、単一のピクセルを変更するSetRGBA64メソッドで、Imageとimage.RGBA64Imageの
+// RGBA64Imageは、単一のピクセルを変更するSetRGBA64メソッドで、[Image] と [image.RGBA64Image] の
 // インターフェースを拡張します。SetRGBA64はSetを呼び出すのと同等ですが、具体的な色の
-// タイプをcolor.Colorインターフェースタイプに変換する際の割り当てを避けることができます。
->>>>>>> release-branch.go1.21
+// タイプを [color.Color] インターフェースタイプに変換する際の割り当てを避けることができます。
 type RGBA64Image interface {
 	image.RGBA64Image
 	Set(x, y int, c color.Color)
@@ -37,11 +30,6 @@ type RGBA64Image interface {
 
 // Quantizerは、画像のパレットを生成します。
 type Quantizer interface {
-<<<<<<< HEAD
-=======
-	// Quantizeは、最大 cap(p) - len(p) の色をpに追加し、mをパレット化された画像に
-	// 変換するための適切なパレットを返します。
->>>>>>> release-branch.go1.21
 	Quantize(p color.Palette, m image.Image) color.Palette
 }
 
@@ -55,36 +43,18 @@ const (
 	Src
 )
 
-<<<<<<< HEAD
-// Draw implements the [Drawer] interface by calling the Draw function with this
-// [Op].
+// Drawは、この [Op] とともにDraw関数を呼び出すことで [Drawer] インターフェースを実装します。
 func (op Op) Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point)
 
-// Drawer contains the [Draw] method.
+// Drawerは [Draw] メソッドを含みます。
 type Drawer interface {
 	Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point)
 }
 
-// FloydSteinberg is a [Drawer] that is the [Src] [Op] with Floyd-Steinberg error
-// diffusion.
+// FloydSteinbergは、Floyd-Steinberg誤差拡散を持つ [Src] [Op] の [Drawer] です。
 var FloydSteinberg Drawer = floydSteinberg{}
 
-// Draw calls [DrawMask] with a nil mask.
-=======
-// Drawは、このOpとともにDraw関数を呼び出すことでDrawerインターフェースを実装します。
-func (op Op) Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point)
-
-// DrawerはDrawメソッドを含みます。
-type Drawer interface {
-	// Drawは、dstのr.Minをsrcのspに揃え、その後dstの矩形rをsrcをdstに描画した結果で置き換えます。
-	Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point)
-}
-
-// FloydSteinbergは、Floyd-Steinberg誤差拡散を持つSrc OpのDrawerです。
-var FloydSteinberg Drawer = floydSteinberg{}
-
-// Drawは、nilマスクでDrawMaskを呼び出します。
->>>>>>> release-branch.go1.21
+// Drawは、nilマスクで [DrawMask] を呼び出します。
 func Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point, op Op)
 
 // DrawMaskは、dstのr.Minをsrcのspとmaskのmpに揃え、その後dstの矩形rを
