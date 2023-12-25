349a581156f582f59803419ecb632caf70176918
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-25 10:26:39 +0900

    math/cmplx: コンフリクト解消

--- math/cmplx/pow.go
+++ math/cmplx/pow.go
@@ -4,13 +4,8 @@
 
 package cmplx
 
-<<<<<<< HEAD
-// Pow returns x**y, the base-x exponential of y.
-// For generalized compatibility with [math.Pow]:
-=======
 // Powはx**y、すなわちyの底xの指数を返します。
-// math.Powとの一般的な互換性のために：
->>>>>>> release-branch.go1.21
+// [math.Pow] との一般的な互換性のために：
 //
 //	Pow(0, ±0)は1+0iを返します
 //	real(c)<0の場合のPow(0, c)は、imag(c)がゼロの場合はInf+0iを返し、それ以外の場合はInf+Inf iを返します。
