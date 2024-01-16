1c7ec28ded9917fdc965508a3b5d24fe08c9efe7
ICHINOSE Shogo <shogo82148@gmail.com>
2024-01-17 00:15:04 +0900

    net/rpc/jsonrpc: コンフリクト解消

--- net/rpc/jsonrpc/client.go
+++ net/rpc/jsonrpc/client.go
@@ -11,18 +11,10 @@ import (
 	"github.com/shogo82148/std/net/rpc"
 )
 
-<<<<<<< HEAD
-// NewClientCodecは、conn上でJSON-RPCを使用して新しいrpc.ClientCodecを返します。
+// NewClientCodecは、conn上でJSON-RPCを使用して新しい [rpc.ClientCodec] を返します。
 func NewClientCodec(conn io.ReadWriteCloser) rpc.ClientCodec
 
-// NewClientは、接続先の一連のサービスへのリクエストを処理する新しいrpc.Clientを返します。
-=======
-// NewClientCodec returns a new [rpc.ClientCodec] using JSON-RPC on conn.
-func NewClientCodec(conn io.ReadWriteCloser) rpc.ClientCodec
-
-// NewClient returns a new [rpc.Client] to handle requests to the
-// set of services at the other end of the connection.
->>>>>>> upstream/master
+// NewClientは、接続先の一連のサービスへのリクエストを処理する新しい [rpc.Client] を返します。
 func NewClient(conn io.ReadWriteCloser) *rpc.Client
 
 // Dialは指定されたネットワークアドレスのJSON-RPCサーバに接続します。
