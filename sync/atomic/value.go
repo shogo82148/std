1a91f1026fe3a854f3355c1a356d36af779e3fc7
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-21 00:04:03 +0900

    sync/atomic: コンフリクト解消

--- sync/atomic/value.go
+++ sync/atomic/value.go
@@ -4,15 +4,9 @@
 
 package atomic
 
-<<<<<<< HEAD
 // Valueは、一貫した型の値のアトミックなロードとストアを提供します。
-// Valueのゼロ値はLoadからnilを返します。
-// Storeが呼び出された後、Valueはコピーしてはなりません。
-=======
-// A Value provides an atomic load and store of a consistently typed value.
-// The zero value for a Value returns nil from [Value.Load].
-// Once [Value.Store] has been called, a Value must not be copied.
->>>>>>> upstream/master
+// Valueのゼロ値は [Value.Load] からnilを返します。
+// [Value.Store] が呼び出された後、Valueはコピーしてはなりません。
 //
 // 最初の使用後、Valueはコピーしてはなりません。
 type Value struct {
@@ -23,15 +17,9 @@ type Value struct {
 // このValueに対してStoreの呼び出しがない場合、nilを返します。
 func (v *Value) Load() (val any)
 
-<<<<<<< HEAD
-// Storeは、Value vの値をvalに設定します。
+// Storeは、[Value] vの値をvalに設定します。
 // 与えられたValueに対するStoreのすべての呼び出しは、同じ具体的な型の値を使用しなければなりません。
 // 不一致の型をStoreするとパニックを引き起こし、Store(nil)も同様です。
-=======
-// Store sets the value of the [Value] v to val.
-// All calls to Store for a given Value must use values of the same concrete type.
-// Store of an inconsistent type panics, as does Store(nil).
->>>>>>> upstream/master
 func (v *Value) Store(val any)
 
 // Swapは新しい値をValueに格納し、前の値を返します。Valueが空の場合はnilを返します。
@@ -40,11 +28,7 @@ func (v *Value) Store(val any)
 // 不一致の型をSwapするとパニックを引き起こし、Swap(nil)も同様です。
 func (v *Value) Swap(new any) (old any)
 
-<<<<<<< HEAD
-// CompareAndSwapは、Valueの比較交換操作を実行します。
-=======
-// CompareAndSwap executes the compare-and-swap operation for the [Value].
->>>>>>> upstream/master
+// CompareAndSwapは、[Value] の比較交換操作を実行します。
 //
 // 与えられたValueに対するCompareAndSwapのすべての呼び出しは、同じ具体的な型の値を使用しなければなりません。
 // 不一致の型をCompareAndSwapするとパニックを引き起こし、CompareAndSwap(old, nil)も同様です。
