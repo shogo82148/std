2e6ce916287449002e5f98c240a33d8b52ee966e
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-20 23:14:49 +0900

    net/http/httptest: コンフリクト解消

--- net/http/httptest/httptest.go
+++ net/http/httptest/httptest.go
@@ -11,15 +11,11 @@ import (
 	"github.com/shogo82148/std/net/http"
 )
 
-<<<<<<< HEAD
-// NewRequestはテスト用に、[http.Handler] に渡すことができる新しい受信サーバーリクエストを返します。
-=======
-// NewRequest wraps NewRequestWithContext using context.Background.
+// NewRequestは、context.Backgroundを使用してNewRequestWithContextをラップします。
 func NewRequest(method, target string, body io.Reader) *http.Request
 
-// NewRequestWithContext returns a new incoming server Request, suitable
-// for passing to an [http.Handler] for testing.
->>>>>>> upstream/master
+// NewRequestWithContextは、新しい着信サーバーリクエストを返します。
+// これはテストのために [http.Handler] に渡すのに適しています。
 //
 // targetはRFC 7230の「要求ターゲット」です。パスまたは絶対URLのいずれかを使用できます。targetが絶対URLの場合、URLからホスト名が使用されます。それ以外の場合は、"example.com"が使用されます。
 //
@@ -33,11 +29,6 @@ func NewRequest(method, target string, body io.Reader) *http.Request
 //
 // NewRequestはエラー時にパニックを発生させます。テストではパニックが許容されるため、使用の便宜上です。
 //
-<<<<<<< HEAD
-// サーバーリクエストではなく、クライアントのHTTPリクエストを生成するには、net/httpパッケージのNewRequest関数を参照してください。
-func NewRequest(method, target string, body io.Reader) *http.Request
-=======
-// To generate a client HTTP request instead of a server request, see
-// the NewRequest function in the net/http package.
+// サーバーリクエストの代わりにクライアントHTTPリクエストを生成するには、
+// net/httpパッケージのNewRequest関数を参照してください。
 func NewRequestWithContext(ctx context.Context, method, target string, body io.Reader) *http.Request
->>>>>>> upstream/master
