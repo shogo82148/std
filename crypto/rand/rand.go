baa2f7aaa5fb0167ff7b09d188642713ce1a794b
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-21 00:42:22 +0900

    crypto/rand: コンフリクト解消

--- crypto/rand/rand.go
+++ crypto/rand/rand.go
@@ -9,24 +9,14 @@ import "github.com/shogo82148/std/io"
 
 // Readerは暗号学的に安全な乱数生成器のグローバルで共有されたインスタンスです。
 //
-<<<<<<< HEAD
-// Linux、FreeBSD、Dragonfly、NetBSD、Solarisでは、Readerは利用可能な場合はgetrandom(2)を、
-// それ以外の場合は/dev/urandomを使用します。
-// OpenBSDとmacOSでは、Readerはgetentropy(2)を使用します。
-// その他のUnix系システムでは、Readerは/dev/urandomから読み取ります。
-// Windowsシステムでは、ReaderはProcessPrng APIを使用します。
-// JS/Wasmでは、ReaderはWeb Crypto APIを使用します。
-// WASIP1/Wasmでは、Readerはwasi_snapshot_preview1のrandom_getを使用します。
-=======
-//   - On Linux, FreeBSD, Dragonfly, and Solaris, Reader uses getrandom(2)
-//     if available, and /dev/urandom otherwise.
-//   - On macOS and iOS, Reader uses arc4random_buf(3).
-//   - On OpenBSD and NetBSD, Reader uses getentropy(2).
-//   - On other Unix-like systems, Reader reads from /dev/urandom.
-//   - On Windows, Reader uses the ProcessPrng API.
-//   - On js/wasm, Reader uses the Web Crypto API.
-//   - On wasip1/wasm, Reader uses random_get from wasi_snapshot_preview1.
->>>>>>> upstream/master
+//   - Linux、FreeBSD、Dragonfly、Solarisでは、Readerは利用可能な場合はgetrandom(2)を使用し、
+//     それ以外の場合は/dev/urandomを使用します。
+//   - macOSとiOSでは、Readerはarc4random_buf(3)を使用します。
+//   - OpenBSDとNetBSDでは、Readerはgetentropy(2)を使用します。
+//   - 他のUnix系システムでは、Readerは/dev/urandomから読み取ります。
+//   - Windowsでは、ReaderはProcessPrng APIを使用します。
+//   - js/wasmでは、ReaderはWeb Crypto APIを使用します。
+//   - wasip1/wasmでは、Readerはwasi_snapshot_preview1からrandom_getを使用します。
 var Reader io.Reader
 
 // Readはio.ReadFullを使ってReader.Readを呼び出すヘルパー関数です。
