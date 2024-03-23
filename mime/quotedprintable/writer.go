f6f5ad6649f1cbba8cd190f120eb3a371fa28c83
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-24 01:06:33 +0900

    mime/quotedprintable: コンフリクト解消

--- mime/quotedprintable/writer.go
+++ mime/quotedprintable/writer.go
@@ -6,11 +6,7 @@ package quotedprintable
 
 import "github.com/shogo82148/std/io"
 
-<<<<<<< HEAD
-// Writerは、io.WriteCloserを実装するquoted-printableライターです。
-=======
-// A Writer is a quoted-printable writer that implements [io.WriteCloser].
->>>>>>> upstream/master
+// Writerは、[io.WriteCloser] を実装するquoted-printableライターです。
 type Writer struct {
 	// バイナリモードでは、ライターの入力を純粋なバイナリとして扱い、
 	// 行末のバイトをバイナリデータとして処理します。
@@ -22,27 +18,14 @@ type Writer struct {
 	cr   bool
 }
 
-<<<<<<< HEAD
-// NewWriterは、wに書き込む新しいWriterを返します。
+// NewWriterは、wに書き込む新しい [Writer] を返します。
 func NewWriter(w io.Writer) *Writer
 
 // Writeは、pをquoted-printableエンコーディングでエンコードし、それを
-// 基礎となるio.Writerに書き込みます。行の長さは76文字に制限されます。
-// エンコードされたバイトは、Writerが閉じられるまで必ずしもフラッシュされません。
+// 基礎となる [io.Writer] に書き込みます。行の長さは76文字に制限されます。
+// エンコードされたバイトは、[Writer] が閉じられるまで必ずしもフラッシュされません。
 func (w *Writer) Write(p []byte) (n int, err error)
 
-// CloseはWriterを閉じ、未書き込みのデータを基礎となるio.Writerにフラッシュしますが、
+// Closeは [Writer] を閉じ、未書き込みのデータを基礎となる [io.Writer] にフラッシュしますが、
 // 基礎となるio.Writerを閉じるわけではありません。
-=======
-// NewWriter returns a new [Writer] that writes to w.
-func NewWriter(w io.Writer) *Writer
-
-// Write encodes p using quoted-printable encoding and writes it to the
-// underlying [io.Writer]. It limits line length to 76 characters. The encoded
-// bytes are not necessarily flushed until the [Writer] is closed.
-func (w *Writer) Write(p []byte) (n int, err error)
-
-// Close closes the [Writer], flushing any unwritten data to the underlying
-// [io.Writer], but does not close the underlying io.Writer.
->>>>>>> upstream/master
 func (w *Writer) Close() error
