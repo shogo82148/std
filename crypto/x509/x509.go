a784dc6b1f2ff93e3c8297b00fbc63b5ed65bebd
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-27 21:12:31 +0900

    crypto/x509: コンフリクト解消

--- crypto/x509/x509.go
+++ crypto/x509/x509.go
@@ -293,20 +293,11 @@ func (h UnhandledCriticalExtension) Error() string
 // テンプレートからのSubjectKeyIdが空で、テンプレートがCAである場合、SubjectKeyIdは
 // 公開鍵のハッシュから生成されます。
 //
-<<<<<<< HEAD
 // PolicyIdentifierとPoliciesフィールドは、両方とも証明書ポリシーOIDをマーシャルするために使用されます。
 // デフォルトでは、PolicyIdentifierのみがマーシャルされますが、
 // GODEBUG設定の"x509usepolicies"が"1"の値を持つ場合、Policiesフィールドが
 // PolicyIdentifierフィールドの代わりにマーシャルされます。Policiesフィールドは、
 // コンポーネントが31ビットより大きいポリシーOIDをマーシャルするために使用できます。
-=======
-// The PolicyIdentifier and Policies fields are both used to marshal certificate
-// policy OIDs. By default, only the PolicyIdentifier is marshaled, but if the
-// GODEBUG setting "x509usepolicies" has the value "1", the Policies field will
-// be marshaled instead of the PolicyIdentifier field. The Policies field can
-// be used to marshal policy OIDs which have components that are larger than 31
-// bits.
->>>>>>> upstream/master
 func CreateCertificate(rand io.Reader, template, parent *Certificate, pub, priv any) ([]byte, error)
 
 // ParseCRLは指定されたバイトからCRLを解析します。PEMエンコードされたCRLがDERエンコードされるべき場所に表示されることがよくありますが、この関数は前方にゴミがない限り、PEMエンコーディングを透過的に処理します。
