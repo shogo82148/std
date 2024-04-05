8af3604089c16637504aebc605a4613b155a209a
ICHINOSE Shogo <shogo82148@gmail.com>
2024-04-05 14:27:03 +0900

    strings: コンフリクト修正

--- strings/compare.go
+++ strings/compare.go
@@ -7,12 +7,6 @@ package strings
 // Compareは、2つの文字列を辞書順で比較して整数を返します。
 // a == bの場合は0、a < bの場合は-1、a > bの場合は+1になります。
 //
-<<<<<<< HEAD
-// Compareは、パッケージbytesとの対称性のために含まれています。
-// 通常、組み込みの文字列比較演算子==、<、>などを使用する方が明確で、常に高速です。
-=======
-// Use Compare when you need to perform a three-way comparison (with
-// slices.SortFunc, for example). It is usually clearer and always faster
-// to use the built-in string comparison operators ==, <, >, and so on.
->>>>>>> upstream/master
+// 3つの比較を行う必要があるとき（例えば、slices.SortFuncと一緒に）にCompareを使用します。
+// 通常、組み込みの文字列比較演算子 ==、<、>などを使用する方が明確で、常に高速です。
 func Compare(a, b string) int
