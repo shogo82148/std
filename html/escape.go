464cc399d25460dac0e86b0a64572699d8bd255b
ICHINOSE Shogo <shogo82148@gmail.com>
2025-10-05 02:56:23 +0900

    html: 翻訳修正

--- html/escape.go
+++ html/escape.go
@@ -5,29 +5,13 @@
 // htmlパッケージは、HTMLテキストのエスケープとアンエスケープのための関数を提供します。
 package html
 
-<<<<<<< HEAD
-// EscapeStringは、"<"のような特殊文字を"&lt;"にエスケープします。
-// それは、<, >, &, ' および " の5つの文字だけをエスケープします。
-// 常にUnescapeString(EscapeString(s)) == sが成り立ちますが、その逆は
-// 常に成り立つわけではありません。
+// EscapeStringは"<"のような特殊文字を"&lt;"にエスケープします。
+// エスケープするのは5つの文字のみです：<、>、&、'、"。
+// [UnescapeString](EscapeString(s)) == sは常に成り立ちますが、逆は常に真ではありません。
 func EscapeString(s string) string
 
-// UnescapeStringは、"&lt;"のようなエンティティを"<"にアンエスケープします。
-// それはEscapeStringがエスケープするよりも広範なエンティティをアンエスケープします。
-// 例えば、"&aacute;"は"á"に、"&#225;"も"&#xE1;"も"á"にアンエスケープします。
-// 常にUnescapeString(EscapeString(s)) == sが成り立ちますが、その逆は
-// 常に成り立つわけではありません。
-=======
-// EscapeString escapes special characters like "<" to become "&lt;". It
-// escapes only five such characters: <, >, &, ' and ".
-// [UnescapeString](EscapeString(s)) == s always holds, but the converse isn't
-// always true.
-func EscapeString(s string) string
-
-// UnescapeString unescapes entities like "&lt;" to become "<". It unescapes a
-// larger range of entities than [EscapeString] escapes. For example, "&aacute;"
-// unescapes to "á", as does "&#225;" and "&#xE1;".
-// UnescapeString([EscapeString](s)) == s always holds, but the converse isn't
-// always true.
->>>>>>> upstream/release-branch.go1.25
+// UnescapeStringは"&lt;"のようなエンティティを"<"にアンエスケープします。
+// [EscapeString]がエスケープするよりも広範囲のエンティティをアンエスケープします。
+// たとえば、"&aacute;"は"á"にアンエスケープされ、"&#225;"や"&#xE1;"も同様です。
+// UnescapeString([EscapeString](s)) == sは常に成り立ちますが、逆は常に真ではありません。
 func UnescapeString(s string) string
