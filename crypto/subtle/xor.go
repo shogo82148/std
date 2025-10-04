d49c582c468dbaa06caa086de09fab099cfaf7b7
ICHINOSE Shogo <shogo82148@gmail.com>
2025-10-05 01:22:50 +0900

    crypto/subtle: XORBytesの翻訳修正

--- crypto/subtle/xor.go
+++ crypto/subtle/xor.go
@@ -4,19 +4,12 @@
 
 package subtle
 
-<<<<<<< HEAD
-// XORBytesはdst[i] = x[i] ^ y[i]をi < n = min(len(x), len(y))のすべてのiに対して設定し、
-// dstに書き込まれたバイト数であるnを返します。
-// もしdstの長さが少なくともnでない場合、
-// XORBytesはなにもdstに書き込まずにパニックを起こします。
-=======
-// XORBytes sets dst[i] = x[i] ^ y[i] for all i < n = min(len(x), len(y)),
-// returning n, the number of bytes written to dst.
+// XORBytesはすべてのi < n = min(len(x), len(y))に対してdst[i] = x[i] ^ y[i]を設定し、
+// dstに書き込まれたバイト数nを返します。
 //
-// If dst does not have length at least n,
-// XORBytes panics without writing anything to dst.
+// dstの長さが少なくともnでない場合、
+// XORBytesはdstに何も書き込まずにパニックします。
 //
-// dst and x or y may overlap exactly or not at all,
-// otherwise XORBytes may panic.
->>>>>>> upstream/release-branch.go1.25
+// dstとxまたはyは完全に重複するか全く重複しないかのいずれかである必要があり、
+// そうでない場合XORBytesはパニックすることがあります。
 func XORBytes(dst, x, y []byte) int
