95c01eeb052fbcc31c8d52b349fe8bf05c06b488
ICHINOSE Shogo <shogo82148@gmail.com>
2025-10-05 14:24:03 +0900

    runtime/pprof: 翻訳修正

--- runtime/pprof/pprof.go
+++ runtime/pprof/pprof.go
@@ -38,18 +38,12 @@
 //	        if err != nil {
 //	            log.Fatal("could not create memory profile: ", err)
 //	        }
-<<<<<<< HEAD
-//	        defer f.Close() // エラーハンドリングは例外です
+//	        defer f.Close() // エラーハンドリングは例のため省略
 //	        runtime.GC() // 最新の統計情報を取得
-//	        if err := pprof.WriteHeapProfile(f); err != nil {
-=======
-//	        defer f.Close() // error handling omitted for example
-//	        runtime.GC() // get up-to-date statistics
-//	        // Lookup("allocs") creates a profile similar to go test -memprofile.
-//	        // Alternatively, use Lookup("heap") for a profile
-//	        // that has inuse_space as the default index.
+//	        // Lookup("allocs")は go test -memprofile と同様のプロファイルを作成します。
+//	        // または、Lookup("heap")を使うことで、
+//	        // デフォルトのインデックスが inuse_space となるプロファイルを取得できます。
 //	        if err := pprof.Lookup("allocs").WriteTo(f, 0); err != nil {
->>>>>>> upstream/release-branch.go1.25
 //	            log.Fatal("could not write memory profile: ", err)
 //	        }
 //	    }
@@ -137,23 +131,10 @@ import (
 // 例えば、他のゴルーチンがロックを取得しようと待っている間に長時間保持されたロックは、
 // ロックが最終的に解除されたとき（つまり、[sync.Mutex.Unlock] で）に競合を報告します。
 //
-<<<<<<< HEAD
-// サンプル値は、他のゴルーチンがロックを待ってブロックされていた累積時間に相当し、
-// [runtime.SetMutexProfileFraction] によって指定されたイベントベースのサンプリングに従います。
-// 例えば、ある呼び出し元がロックを1秒間保持していて、他の5つのゴルーチンがそのロックを獲得するために
-// その全秒を待っていた場合、そのアンロックのコールスタックは5秒の競合を報告します。
-//
-// ランタイム内部のロックは常に "runtime._LostContendedRuntimeLock" の位置で報告されます。ランタイム内部のロックに対する
-// より詳細なスタックトレースは、`GODEBUG=runtimecontentionstacks=1` を設定することで取得できます（注意事項については
-// [runtime] パッケージのドキュメントを参照してください）。
-=======
-// Sample values correspond to the approximate cumulative time other goroutines
-// spent blocked waiting for the lock, subject to event-based sampling
-// specified by [runtime.SetMutexProfileFraction]. For example, if a caller
-// holds a lock for 1s while 5 other goroutines are waiting for the entire
-// second to acquire the lock, its unlock call stack will report 5s of
-// contention.
->>>>>>> upstream/release-branch.go1.25
+// サンプル値は、他のゴルーチンがロックの取得を待ってブロックされた累積時間のおおよその合計を表します。
+// この値は [runtime.SetMutexProfileFraction] で指定されたイベントベースのサンプリングに従います。
+// 例えば、呼び出し元が1秒間ロックを保持している間に、5つの他のゴルーチンがその全期間ロックの取得を待っていた場合、
+// アンロック時のスタックトレースには5秒分の競合が報告されます。
 type Profile struct {
 	name  string
 	mu    sync.Mutex
