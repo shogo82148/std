6ec5a7c99216c68f92dcefcd6d915b9f0ec94e93
ICHINOSE Shogo <shogo82148@gmail.com>
2026-04-21 23:32:34 +0900

    crypto/rand/util.go: コンフリクト解消

--- crypto/rand/util.go
+++ crypto/rand/util.go
@@ -9,19 +9,13 @@ import (
 	"github.com/shogo82148/std/math/big"
 )
 
-<<<<<<< HEAD
-// Primeは与えられたビット長で高い確率で素数である数を返します。
-// Primeはrand.Readが返すエラーやbits < 2の場合にエラーを返します。
-func Prime(rand io.Reader, bits int) (*big.Int, error)
-=======
-// Prime returns a number of the given bit length that is prime with high probability.
-// Prime will return error for any error returned by rand.Read or if bits < 2.
+// Primeは、指定されたビット長で高い確率で素数である数値を返します。
+// Primeは、rand.Readが返すエラーまたはbits < 2の場合にエラーを返します。
 //
-// Since Go 1.26, a secure source of random bytes is always used, and the Reader is
-// ignored unless GODEBUG=cryptocustomrand=1 is set. This setting will be removed
-// in a future Go release. Instead, use [testing/cryptotest.SetGlobalRandom].
+// Go 1.26以降、安全なランダムバイトソースが常に使用され、GODEBUG=cryptocustomrand=1が
+// 設定されない限りReaderは無視されます。この設定は将来のGoリリースで削除されます。
+// 代わりに [testing/cryptotest.SetGlobalRandom] を使用してください。
 func Prime(r io.Reader, bits int) (*big.Int, error)
->>>>>>> upstream/release-branch.go1.26
 
 // Intは[0, max)の範囲の一様乱数値を返します。max <= 0の場合はパニックし、
 // rand.Readがエラーを返した場合はエラーを返します。
