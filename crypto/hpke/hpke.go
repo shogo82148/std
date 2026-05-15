f7aa728e3fdeae7471124e92a401e4cb7e48f894
ICHINOSE Shogo <shogo82148@gmail.com>
2026-05-15 22:04:04 +0900

    crypto/hpke: hpke.go 翻訳

--- crypto/hpke/hpke.go
+++ crypto/hpke/hpke.go
@@ -2,74 +2,76 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package hpke implements Hybrid Public Key Encryption (HPKE) as defined in
-// [RFC 9180].
+// Package hpke は [RFC 9180] で定義されたハイブリッド公開鍵暗号化 (HPKE)
+// を実装します。
 //
 // [RFC 9180]: https://www.rfc-editor.org/rfc/rfc9180.html
 package hpke
 
-// Sender is a sending HPKE context. It is instantiated with a specific KEM
-// encapsulation key (i.e. the public key), and it is stateful, incrementing the
-// nonce counter for each [Sender.Seal] call.
+// Sender は送信 HPKE コンテキストです。特定の KEM
+// カプセル化鍵、つまり公開鍵で作成され、ステートフルで
+// 各 [Sender.Seal] 呼び出しで的確にノンスカウンターを増やします。
 type Sender struct {
 	*context
 }
 
-// Recipient is a receiving HPKE context. It is instantiated with a specific KEM
-// decapsulation key (i.e. the secret key), and it is stateful, incrementing the
-// nonce counter for each successful [Recipient.Open] call.
+// Recipient は受信 HPKE コンテキストです。特定の KEM
+// カプセル化解除鍵、つまり秘密鍵で作成され、ステートフルで
+// 各成功した [Recipient.Open] 呼び出しで的確にノンスカウンターを増やします。
 type Recipient struct {
 	*context
 }
 
-// NewSender returns a sending HPKE context for the provided KEM encapsulation
-// key (i.e. the public key), and using the ciphersuite defined by the
-// combination of KEM, KDF, and AEAD.
+// NewSender は、提供された KEM カプセル化鍵、つまり公開鍵のための
+// 送信 HPKE コンテキストを返します。KEM、KDF、AEAD の組み合わせで
+// 定義された暗号スイートを使用します。
 //
-// The info parameter is additional public information that must match between
-// sender and recipient.
+// info パラメーターは、送信者と受信者間で一致しなくてはいけない
+// 追加公開情報です。
 //
-// The returned enc ciphertext can be used to instantiate a matching receiving
-// HPKE context with the corresponding KEM decapsulation key.
+// 返された enc 暗号文は、対応する KEM カプセル化解除鍵を
+// 履行化するために、対応する受信 HPKE コンテキストを
+// 使用できます。
 func NewSender(pk PublicKey, kdf KDF, aead AEAD, info []byte) (enc []byte, s *Sender, err error)
 
-// NewRecipient returns a receiving HPKE context for the provided KEM
-// decapsulation key (i.e. the secret key), and using the ciphersuite defined by
-// the combination of KEM, KDF, and AEAD.
+// NewRecipient は、提供された KEM カプセル化解除鍵、つまり秘密鍵のための
+// 受信 HPKE コンテキストを返します。KEM、KDF、AEAD の組み合わせで
+// 定義された暗号スイートを使用します。
 //
-// The enc parameter must have been produced by a matching sending HPKE context
-// with the corresponding KEM encapsulation key. The info parameter is
-// additional public information that must match between sender and recipient.
+// enc パラメーターは、対応する KEM カプセル化鍵を持つ一致した
+// 送信 HPKE コンテキストで作成された必要があります。info パラメーターは
+// 送信者と受信者間で一致しなくてはいけない追加公開情報です。
 func NewRecipient(enc []byte, k PrivateKey, kdf KDF, aead AEAD, info []byte) (*Recipient, error)
 
-// Seal encrypts the provided plaintext, optionally binding to the additional
-// public data aad.
+// Seal は、提供された平文を暗号化し、理想的に追加公開データ aad に
+// 紅付けします。
 //
-// Seal uses incrementing counters for each call, and Open on the receiving side
-// must be called in the same order as Seal.
+// Seal は各呼び出しに従ってノンスカウンターを使用し、受信側の Open は
+// Seal と同じ順序で呼び出す必要があります。
 func (s *Sender) Seal(aad, plaintext []byte) ([]byte, error)
 
-// Seal instantiates a single-use HPKE sending HPKE context like [NewSender],
-// and then encrypts the provided plaintext like [Sender.Seal] (with no aad).
-// Seal returns the concatenation of the encapsulated key and the ciphertext.
+// Seal は [NewSender] のように一回限りの送信 HPKE コンテキストを初期化し
+// てから、[Sender.Seal] のように提供された平文を暗号化します（aad なし）。
+// Seal はカプセル化鍵と暗号文の連結を返します。
 func Seal(pk PublicKey, kdf KDF, aead AEAD, info, plaintext []byte) ([]byte, error)
 
-// Export produces a secret value derived from the shared key between sender and
-// recipient. length must be at most 65,535.
+// Export は、送信者と受信者間でを一撤する共有鍵から導出した
+// 秘密値を生成します。length は 65535 以下である必要があります。
 func (s *Sender) Export(exporterContext string, length int) ([]byte, error)
 
-// Open decrypts the provided ciphertext, optionally binding to the additional
-// public data aad, or returns an error if decryption fails.
+// Open は、提供された暗号文を複号化し、理想的に追加公開データ aad に
+// 紅付けします。複号化が失敗した場合、エラーを返します。
 //
-// Open uses incrementing counters for each successful call, and must be called
-// in the same order as Seal on the sending side.
+// Open は各成功した呼び出しに従ってノンスカウンターを使用し、
+// 送信側の Seal と同じ順序で呼び出す必要があります。
 func (r *Recipient) Open(aad, ciphertext []byte) ([]byte, error)
 
-// Open instantiates a single-use HPKE receiving HPKE context like [NewRecipient],
-// and then decrypts the provided ciphertext like [Recipient.Open] (with no aad).
-// ciphertext must be the concatenation of the encapsulated key and the actual ciphertext.
+// Open は [NewRecipient] のように一回限りの受信 HPKE コンテキストを
+// 初期化してから、[Recipient.Open] のように提供された暗号文を複号化します
+// （aad なし）。ciphertext はカプセル化鍵と実際の暗号文の連結である
+// 必要があります。
 func Open(k PrivateKey, kdf KDF, aead AEAD, info, ciphertext []byte) ([]byte, error)
 
-// Export produces a secret value derived from the shared key between sender and
-// recipient. length must be at most 65,535.
+// Export は、送信者と受信者間でを一撤する共有鍵から導出した
+// 秘密値を生成します。length は 65535 以下である必要があります。
 func (r *Recipient) Export(exporterContext string, length int) ([]byte, error)
