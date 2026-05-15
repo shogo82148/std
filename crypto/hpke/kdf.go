28f3b154442b69dbf68df2d2a12b66c02c814fe9
ICHINOSE Shogo <shogo82148@gmail.com>
2026-05-15 22:04:51 +0900

    crypto/hpke: kdf.go 翻訳

--- crypto/hpke/kdf.go
+++ crypto/hpke/kdf.go
@@ -4,8 +4,8 @@
 
 package hpke
 
-// The KDF is one of the three components of an HPKE ciphersuite, implementing
-// key derivation.
+// KDF は HPKE 暗号スイートの 3 つのコンポーネントの 1 つで、鍵導出を
+// 実装します。
 type KDF interface {
 	ID() uint16
 	oneStage() bool
@@ -15,23 +15,23 @@ type KDF interface {
 	labeledExpand(suiteID, randomKey []byte, label string, info []byte, length uint16) ([]byte, error)
 }
 
-// NewKDF returns the KDF implementation for the given KDF ID.
+// NewKDF は与えられた KDF ID の KDF 実装を返します。
 //
-// Applications are encouraged to use specific implementations like [HKDFSHA256]
-// instead, unless runtime agility is required.
+// アプリケーションは、ランタイム可変性が必要でない限り、
+// [HKDFSHA256] などの特定の実装を使用してください。
 func NewKDF(id uint16) (KDF, error)
 
-// HKDFSHA256 returns an HKDF-SHA256 KDF implementation.
+// HKDFSHA256 は HKDF-SHA256 KDF 実装を返します。
 func HKDFSHA256() KDF
 
-// HKDFSHA384 returns an HKDF-SHA384 KDF implementation.
+// HKDFSHA384 は HKDF-SHA384 KDF 実装を返します。
 func HKDFSHA384() KDF
 
-// HKDFSHA512 returns an HKDF-SHA512 KDF implementation.
+// HKDFSHA512 は HKDF-SHA512 KDF 実装を返します。
 func HKDFSHA512() KDF
 
-// SHAKE128 returns a SHAKE128 KDF implementation.
+// SHAKE128 は SHAKE128 KDF 実装を返します。
 func SHAKE128() KDF
 
-// SHAKE256 returns a SHAKE256 KDF implementation.
+// SHAKE256 は SHAKE256 KDF 実装を返します。
 func SHAKE256() KDF
